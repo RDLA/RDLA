623fcb133b4795cb84d76a3b33a66f3fb036acb3
Damien Brian <damien.brian@rdla.fr>
2011-08-28 12:18:02 +0200

    Initialize rails apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RDLA::Application.config.secret_token = '67dd0e32b877f432dfad17d78be59004ec972154d2b3489dd79f916661018844e1cc605b919fb5eedf7f5bd114f1d1a35ea72be075583903477849eab3bbec9a'
