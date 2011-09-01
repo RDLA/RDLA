01be6429309de52c972c07d09b7c5b70b6afafcd
Damien Brian <damien.brian@rdla.fr>
2011-09-01 13:16:01 +0200

    Fix bug in model by adding map_id and user_id as accessible attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  attr_accessible :map, :user, :name, :posx, :posy
+  attr_accessible :map, :user, :name, :posx, :posy, :map_id, :user_id
   
   belongs_to :map
   belongs_to :user
