5f726895a1956bde964262d8956d3f13187048e6
Damien Brian <damien.brian@rdla.fr>
2011-09-12 17:20:26 +0200

    Fix bad assignation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,8 +2,10 @@ class Feedback < ActiveRecord::Base
   
   validates :status, :inclusion => { :in => %w(waiting completed in_process)}
   validates :category, :inclusion => { :in => %w(feature bug todo)}
+   
   belongs_to :user
-  belongs_to :user, :foreign_key => "worker"
+  belongs_to :worker, :foreign_key => "worker", :class_name => "User" 
+ 
   scope :completed, where("status='completed'")
   scope :waiting, where("status='waiting'")
   scope :in_process, where("status='in_process'")
