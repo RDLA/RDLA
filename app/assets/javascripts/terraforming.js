3329745d5d92559e0848bac0f2001eec66fc97ce
Damien Brian <damien.brian@rdla.fr>
2011-10-02 10:55:07 +0200

    Update minimap when create a new field

--- app/assets/javascripts/terraforming.js
+++ app/assets/javascripts/terraforming.js
@@ -2,15 +2,15 @@ $(function(){
    
     if(window.location.pathname == '/admin/terraformings')
     {
-         $("body").keypress(function(event) 
-    {
+        $("body").keypress(function(event) 
+        {
      
-      if ( event.which == 109 ) 
-      {
-        $("#minimap").toggle();
-        event.preventDefault();
-       }
-    });
+            if ( event.which == 109 ) 
+            {
+                $("#minimap").toggle();
+                event.preventDefault();
+            }
+        });
         /* Category */
         $(".terraform_biblio_item").hide();
         $("#cat1").show();
@@ -18,8 +18,8 @@ $(function(){
         $("#category_id").livequery('change', function()
         {
             category_id = $("#category_id").val();
-        $(".terraform_biblio_item").hide();
-        $("#cat"+category_id).show();
+            $(".terraform_biblio_item").hide();
+            $("#cat"+category_id).show();
 
         })
         
@@ -38,10 +38,20 @@ $(function(){
             $(this).css("opacity","0.5");
             console.log("Ajax request for creating a field (#"+field+") at position["+pos[0]+";"+pos[1]+"] on #"+map+"");
             
-            $.post('/admin/terraformings',{ posx: pos[0], posy: pos[1], map: map, field:field }, function(data){
+            $.post('/admin/terraformings',{
+                posx: pos[0], 
+                posy: pos[1], 
+                map: map, 
+                field:field
+            }, function(data){
                 $("#map").html(data);
                 $(this).css("opacity","1");
-                 
+                 //Update minimap
+        $.get("/admin/terraformings/minimap_js.js", function(data){
+            
+            eval(data);
+        });
+        
             });
             
             
@@ -67,7 +77,7 @@ $(function(){
             if(confirm("Voulez-vous vraiment colorier toutes les cases visibles?"))
             {
                 $("#build_all").val("Coloriage en cours...");
-               x = $("#posx").val();
+                x = $("#posx").val();
                 y = $("#posy").val();      
                 field = $("#selectedPictureId").val();
                 map = $("#map_id").val();
@@ -84,46 +94,55 @@ $(function(){
                     success: function(data) {
                         $("#map").html(data);
                         $("#build_all").val("Colorier toutes les cases visibles");
-               
+                        //Update minimap
+                        $.get("/admin/terraformings/minimap_js.js", function(data){
+            
+                            eval(data);
+                        });
+            
                     },
-                    error: function(data) {alert("Erreur"); }
+                    error: function(data) {
+                        alert("Erreur");
+                    }
                 
                  
-            });
-        }
-    });
-}
+                });
+            }
+        });
+    }
 });
 count_uniq = 0;
 function update_params()
 {
-map = $("#map_id").val();
+    map = $("#map_id").val();
 
-x = $("#posx").val();
-y = $("#posy").val();
-width =$("#width").val();
-height = $("#height").val();
-console.log("Ajax request for uploading params and refresh map| Position["+x+";"+y+"] on #"+map+" with windows("+width+"x"+height+") ");
-$("#majinfo").val("Mise à jour...");
-$.ajax({
-    url:"/admin/terraformings/update_position",
-    type: "PUT",
-    dataType: "html",
-    data: "map_id="+map+"&posx="+x+"&posy="+y+"&width="+width+"&height="+height,
-    success: function(data) {
-        $("#majinfo").val("Modifier");
-        $("#map").html(data);
-        $("#map").css("width",72*(parseInt(width,10)+1)+"px");
+    x = $("#posx").val();
+    y = $("#posy").val();
+    width =$("#width").val();
+    height = $("#height").val();
+    console.log("Ajax request for uploading params and refresh map| Position["+x+";"+y+"] on #"+map+" with windows("+width+"x"+height+") ");
+    $("#majinfo").val("Mise à jour...");
+    $.ajax({
+        url:"/admin/terraformings/update_position",
+        type: "PUT",
+        dataType: "html",
+        data: "map_id="+map+"&posx="+x+"&posy="+y+"&width="+width+"&height="+height,
+        success: function(data) {
+            $("#majinfo").val("Modifier");
+            $("#map").html(data);
+            $("#map").css("width",72*(parseInt(width,10)+1)+"px");
                   
-        $("#map").css("height",72*(parseInt(height,10)+1)+"px");
-        //Update minimap
-        $.get("/admin/terraformings/minimap_js.js", function(data){
+            $("#map").css("height",72*(parseInt(height,10)+1)+"px");
+            //Update minimap
+            $.get("/admin/terraformings/minimap_js.js", function(data){
             
-            eval(data);
-        })
+                eval(data);
+            });
         
        
-    },
-    error: function(data) {alert("Erreur"); }
-});
+        },
+        error: function(data) {
+            alert("Erreur");
+        }
+    });
 }   
